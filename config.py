f67423856f61fa6e4eb1154e2948fae7e66f25fc
Hyper4576 <164305639+Hyper4576@users.noreply.github.com>
2024-03-24 10:45:27 +0530

    Hyper 4.O

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SHIVAM1466")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+BlF81x_Uid8xZjI1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/HYPER_X_WORLD_SUPPORT")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
